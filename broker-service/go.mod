fc8e337905925a74b50fa2b5438174dc546cda01
= <sharukhprogramming@gmail.com>
2025-08-25 10:23:44 +0530

    done until make file part

---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module broker
+
+go 1.24.2
+
+require (
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/go-chi/cors v1.2.2
+)
---
+++ front-end/cmd/web/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		render(w, "test.page.gohtml")
+	})
+
+	fmt.Println("Starting front end service on port 80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func render(w http.ResponseWriter, t string) {
+
+	//partial file names 
+	partials := []string{
+		"./cmd/web/templates/base.layout.gohtml",
+		"./cmd/web/templates/header.partial.gohtml",
+		"./cmd/web/templates/footer.partial.gohtml",
+	}
+
+	//slice to store all files 
+	var templateSlice []string
+	// add the main page first to the slice the main page act as 
+	// entry point while parsing the file 
+	templateSlice = append(templateSlice, fmt.Sprintf("./cmd/web/templates/%s", t))
+
+	for _, x := range partials {
+		templateSlice = append(templateSlice, x)
+	}
+
+	// parsing the files using html/template module 
+	tmpl, err := template.ParseFiles(templateSlice...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//execute method will write the template to response writer 
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
